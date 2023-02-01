6406b77e893a13746d5ca3bd650dfdf0a3d753f8
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-02-01 20:02:08 +0200

    ,

--- src/components/Loader/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Rings } from 'react-loader-spinner';
-
-function Loader() {
-  return <Rings color="red" height={100} width={100} />;
-}
-
-export default Loader;
--- src/pages/HomePage/HomePage.js
+++
@@ -1,5 +0,0 @@
-import styles from './HomePage.module.css';
-
-export default function HomePage() {
-    return <h1 className={styles.title}>Welcome!</h1>
-};
